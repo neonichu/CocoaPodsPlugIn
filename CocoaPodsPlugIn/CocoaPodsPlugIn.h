2143c0155155960878dc0831c2672d5747298dc2
Boris Bügling <boris@icculus.org>
2013-09-19 13:40:28 +0200

    Initial Commit

---
+++ CocoaPodsPlugIn/CocoaPodsPlugIn.h
@@ -0,0 +1,13 @@
+//
+//  CocoaPodsPlugIn.h
+//  CocoaPodsPlugIn
+//
+//  Created by Boris Bügling on 19.09.13.
+//  Copyright (c) 2013 Boris Bügling. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface CocoaPodsPlugIn : NSObject
+
+@end
